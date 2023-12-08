178c6c64d1a0fa60114aeeeb5e6e5a614834e5b1
Ella Bryning <ellabryning@gmail.com>
2023-12-08 21:37:49 +1100

    movement

--- game.js
+++ game.js
@@ -5,6 +5,26 @@ var ballY = 50;
 var ballSpeedX = 15;
 var ballSpeedY = 5;
 
+var paddle1Y = 250;
+const paddleHeight = 100;
+
+window.onload = function() {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    var framesPerSecond = 30;
+    setInterval(function () {
+        drawEverything();
+        moveEverything();
+    }, 1000/framesPerSecond);
+
+    canvas.addEventListener('mousemove',
+        (evt) => {
+            var mousePosition = calculateMousePosition(evt);
+            paddle1Y = mousePosition.y-(paddleHeight/2);
+        }
+    )
+};
+
 function createRect (leftX, topY, width, height, colour) {
     canvasContext.fillStyle = colour;
     canvasContext.fillRect(leftX, topY, width, height); 
@@ -17,29 +37,44 @@ function createCircle (centerX, centerY, radius, colour) {
     canvasContext.fill();
 };
 
+function calculateMousePosition (evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+        return {
+            x: mouseX,
+            y: mouseY
+        }
+};  
 
-window.onload = function() {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
-    var framesPerSecond = 30;
-    setInterval(function () {
-        drawEverything();
-        moveEverything();
-    }, 1000/framesPerSecond);
+function ballReset() {
+    ballX = ballX + ballSpeedX;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
 };
 
 function moveEverything () {
     ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
+
+    if(ballX < 0) {
+      ballSpeedX = -ballSpeedX;
+    }
     if(ballX > canvas.width) {
         ballSpeedX = -ballSpeedX;
     }
-    if(ballX < 0) {
-        ballSpeedX = -ballSpeedX;
+    if(ballY > canvas.height) {
+        ballSpeedY = -ballSpeedY;
     }
-};
+    if(ballY < 0) {
+        ballSpeedY = -ballSpeedY;
+    }
+}
 
 function drawEverything () {
     createRect(0, 0, canvas.width, canvas.height, 'black'); //Canvas
-    createRect(0, 210, 10, 10, 'white'); //Left player paddle
-    createCircle(ballX, 150, 10, 'green'); // Ball
+    createRect(0, paddle1Y, 10, paddleHeight, 'white'); //Left player paddle
+    //createRect(0, paddle2Y, 10, paddleHeight, 'white'); //Right computer paddle
+    createCircle(ballX, ballY, 10, 'white'); // Ball
 };
